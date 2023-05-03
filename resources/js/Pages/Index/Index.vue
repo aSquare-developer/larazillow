29b197c284e5e1ae84d8d28712d075896d4ed233
Artur Anissimov <arturanissimov@Arturs-MacBook-Air.local>
2023-05-03 15:59:00 +0300

    Create first page

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>Index</div>
+    <Link href="/show">Show page</Link>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        Show
+    </div>
+    <Link href="/">Index page</Link>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
