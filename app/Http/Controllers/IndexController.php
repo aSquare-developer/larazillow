fb7791c94a8708531abbf88cf213be5ddbaf0120
Artur-MacBook-Air <artur@example.com>
2023-05-18 18:01:06 +0300

    Updating and Inserting Data, Mass Assignment

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Listings;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index() 
     {
+        dd(Listings::all());
         return inertia('Index/Index', ['message' => 'Hello this is data from controller!']);
     }
 
