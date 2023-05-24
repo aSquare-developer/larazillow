c4da5eb5d773f2e9d5e77770ae3ea3f804a4bddc
Artur-MacBook-Air <artur@example.com>
2023-05-24 16:30:15 +0300

    Custom UI Components and Grid Responsive Design

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,14 +1,10 @@
 <template>
-    <div>Index</div>
-    <Link href="/show">Show page</Link>
     <div>
         Here is message: {{ message }}
     </div>
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/vue3';
-
 defineProps({
     message: String
 })
