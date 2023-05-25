f337a119cf1521048c2dddf453c4a84c35fb9a2a
Artur-MacBook-Air <artur@example.com>
2023-05-25 16:54:14 +0300

    Edit Form Styling

--- resources/js/Pages/Listing/Edit.vue
+++ resources/js/Pages/Listing/Edit.vue
@@ -1,87 +1,95 @@
 <template>
     <form @submit.prevent="update">
-        <div>
-            <label for="beds">Beds</label>
-            <input v-model.number="form.beds" id="beds" type="text" />
-            <div v-if="form.errors.beds" class="error-message">
-                {{ form.errors.beds }}
+        <div class="grid grid-cols-6 gap-4">
+            <div class="col-span-2">
+                <label class="label">Beds</label>
+                <input v-model.number="form.beds" type="text" class="input" />
+                <div v-if="form.errors.beds">
+                    {{ form.errors.beds }}
+                </div>
             </div>
-        </div>
-        <div>
-            <label for="baths">Baths</label>
-            <input v-model.number="form.baths" id="baths" type="text" />
-            <div v-if="form.errors.baths" class="error-message">
-                {{ form.errors.baths }}
-            </div>
-        </div>
-        <div>
-            <label for="area">Area</label>
-            <input v-model.number="form.area" id="area" type="text" />
-            <div v-if="form.errors.area" class="error-message">
-                {{ form.errors.area }}
-            </div>
-        </div>
-        <div>
-            <label for="city">City</label>
-            <input v-model="form.city" id="city" type="text" />
-            <div v-if="form.errors.city" class="error-message">
-                {{ form.errors.city }}
-            </div>
-        </div>
-        <div>
-            <label for="post_code">Post Code</label>
-            <input v-model="form.code" id="post_code" type="text" />
-            <div v-if="form.errors.code" class="error-message">
-                {{ form.errors.code }}
-            </div>
-        </div>
-        <div>
-            <label for="street">Street</label>
-            <input v-model="form.street" id="street" type="text" />
-            <div v-if="form.errors.street" class="error-message">
-                {{ form.errors.street }}
-            </div>
-        </div>
-        <div>
-            <label for="street_nr">Street Nr</label>
-            <input v-model.number="form.street_nr" id="street_nr" type="text" />
-            <div v-if="form.errors.street_nr" class="error-message">
-                {{ form.errors.street_nr }}
-            </div>
-        </div>
-        <div>
-            <label for="price">Price</label>
-            <input v-model.number="form.price" id="price" type="text" />
-            <div v-if="form.errors.price" class="error-message">
-                {{ form.errors.price }}
-            </div>
-        </div>
 
-        <div>
-            <button type="submit">Edit</button>
+            <div class="col-span-2">
+                <label class="label">Baths</label>
+                <input v-model.number="form.baths" type="text" class="input" />
+                <div v-if="form.errors.baths">
+                    {{ form.errors.baths }}
+                </div>
+            </div>
+
+            <div class="col-span-2">
+                <label class="label">Area</label>
+                <input v-model.number="form.area" type="text" class="input" />
+                <div v-if="form.errors.area">
+                    {{ form.errors.area }}
+                </div>
+            </div>
+
+            <div class="col-span-4">
+                <label class="label">City</label>
+                <input v-model="form.city" type="text" class="input" />
+                <div v-if="form.errors.city">
+                    {{ form.errors.city }}
+                </div>
+            </div>
+
+            <div class="col-span-2">
+                <label class="label">Post Code</label>
+                <input v-model="form.code" type="text" class="input" />
+                <div v-if="form.errors.code">
+                    {{ form.errors.code }}
+                </div>
+            </div>
+
+            <div class="col-span-4">
+                <label class="label">Street</label>
+                <input v-model="form.street" type="text" class="input" />
+                <div v-if="form.errors.street">
+                    {{ form.errors.street }}
+                </div>
+            </div>
+
+            <div class="col-span-2">
+                <label class="label">Street Nr</label>
+                <input v-model.number="form.street_nr" type="text" class="input" />
+                <div v-if="form.errors.street_nr">
+                    {{ form.errors.street_nr }}
+                </div>
+            </div>
+
+            <div class="col-span-6">
+                <label class="label">Price</label>
+                <input v-model.number="form.price" type="text" class="input" />
+                <div v-if="form.errors.price">
+                    {{ form.errors.price }}
+                </div>
+            </div>
+
+            <div class="col-span-6">
+                <button type="submit" class="btn-primary">Edit</button>
+            </div>
         </div>
     </form>
 </template>
 
 <script setup>
-import {router, useForm} from "@inertiajs/vue3";
+import { useForm } from "@inertiajs/vue3";
+
 
 const props = defineProps({
-    listing: Object
+    listing: Object,
 })
-
 const form = useForm({
-    beds: 0,
-    baths: 0,
-    area: 0,
-    city: null,
-    street: null,
-    code: null,
-    street_nr: null,
-    price: 0,
+    beds: props.listing.beds,
+    baths: props.listing.baths,
+    area: props.listing.area,
+    city: props.listing.city,
+    street: props.listing.street,
+    code: props.listing.code,
+    street_nr: props.listing.street_nr,
+    price: props.listing.price,
 })
-
-const update = () => form.put(route('listing.update', props.listing.id))
+const update = () => form.put(route('listing.update', {listing: props.listing.id}))
 </script>
 
 <style scoped>
@@ -90,14 +98,6 @@ label {
 }
 
 div {
-    padding: 2px;
-}
-
-.error-message {
-    background-color: red;
-    color: white;
-    border-radius: 10px;
+    padding: 2px
 }
 </style>
-
-
