35a9fc39173e34e2edd00de3be65165f6b22df23
Artur-MacBook-Air <artur@example.com>
2023-05-30 17:11:58 +0300

    Authentication Form Markup and Basic Logic

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class AuthController extends Controller
 {
     public function create() {
-
+        return inertia('Auth/Login');
     }
 
     public function store() {
