2b27a42f059f8b558c0293a585ec2dd128e8af53
Artur-MacBook-Air <artur@example.com>
2023-06-20 19:48:02 +0300

    Form Links

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -13,6 +13,12 @@
             </div>
             <div class="mt-4">
                 <button class="btn-primary w-full" type="submit">Login</button>
+
+                <div class="mt-2 text-center">
+                    <Link :href="route('register.create')" class="text-sm text-gray-500">
+                        Need an account? Click here
+                    </Link>
+                </div>
             </div>
         </div>
     </form>
@@ -20,7 +26,7 @@
 </template>
 
 <script setup>
-import {useForm} from "@inertiajs/vue3";
+import { useForm, Link } from "@inertiajs/vue3";
 
 const form = useForm({
     email: null,
